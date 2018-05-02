201025d3c7f0994468b0e0e61997b3d4b9dca1b0
zoltanpataki <zolee7777@gmail.com>
2018-05-02 11:16:33 +0200

    api is working finally

--- src/main/java/com.codecool.sportSite/Controller/NewsController.java
+++ src/main/java/com.codecool.sportSite/Controller/NewsController.java
@@ -1,7 +1,22 @@
 package com.codecool.sportSite.Controller;
 
+import com.codecool.sportSite.Service.NewsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 public class NewsController {
+
+    @Autowired
+    NewsService newsService;
+
+    @GetMapping(value = "new-news")
+    public ResponseEntity getNews() throws IOException{
+        return new ResponseEntity<>(newsService.makeUrl(), HttpStatus.OK);
+    }
 }
--- src/main/java/com.codecool.sportSite/Service/NewsService.java
+++ src/main/java/com.codecool.sportSite/Service/NewsService.java
@@ -1,7 +1,22 @@
 package com.codecool.sportSite.Service;
 
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class NewsService {
+    private final String url = "https://newsapi.org/v2/top-headlines?sources=bbc-sport&apiKey=81ae151ea996440ba155436c41d1174f";
+    public Map<String, String> makeUrl() throws IOException{
+        System.setProperty("http.agent", "Chrome");
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(JsonParser.Feature.ALLOW_BACKSLASH_ESCAPING_ANY_CHARACTER, true);
+        Map<String, String> myMap = mapper.readValue(new URL(url), HashMap.class);
+        return myMap;
+    }
 }
